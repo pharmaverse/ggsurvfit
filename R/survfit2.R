a27d13fe38b0135251723d2c6506ec6b313062f5
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-08-06 19:49:51 -0400

    added a section about survfit2

--- R/survfit2.R
+++ R/survfit2.R
@@ -10,6 +10,40 @@
 #' @inheritParams survival::survfit.formula
 #' @inheritDotParams survival::survfit.formula
 #'
+#' @section `survfit2()` vs `survfit()`:
+#'
+#' Both functions have identical inputs, so why do we need `survfit2()`?
+#'
+#' The *only* difference between `survfit2()` and `survival::survfit()` is that the
+#' former tracks the environment from which the call to the function was made.
+#'
+#' The definition of `survfit2()` is unremarkably simple:
+#'
+#' ```r
+#' survfit2 <- function(formula, ...) {
+#'   # construct survfit object
+#'   survfit <- survival::survfit(formula, ...)
+#'
+#'   # add the environment
+#'   survfit$.Environment = rlang::current_env()
+#'
+#'   # add class and return
+#'   class(survfit) <- c("survfit2", "survfit")
+#'   survfit
+#' }
+#' ```
+#'
+#' The environment is needed to ensure the survfit call can be accurately
+#' reconstructed or parsed at any point post estimation.
+#' The call is parsed when p-values are reported and when labels are created.
+#' For example, the raw variable names appear in the output of a stratified
+#' `survfit()` result, e.g. `"sex=Female"`. When using `survfit2()`, the
+#' originating data frame and formula may be parsed and the raw variable
+#' names removed.
+#'
+#' Most functions in the package work with both `survfit2()` and `survfit()`;
+#' however, the output will be styled in a preferable format with `survfit2()`.
+#'
 #' @return surfit2 object
 #' @export
 #'
