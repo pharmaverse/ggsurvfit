aed3087e81e02da4e6bdb4ea9e95c5cd494914e0
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-08-08 21:48:40 -0400

    ugh

--- R/utils-strata_as_color_block.R
+++ R/utils-strata_as_color_block.R
@@ -7,25 +7,40 @@
 
   # find the colors used in the figure
   colors <-
-    plot_build$data[[1]] |>
-    dplyr::select(.data$colour) |>
-    dplyr::distinct() |>
+    plot_build$data[[1]] %>%
+    dplyr::select(.data$colour) %>%
+    dplyr::distinct() %>%
     dplyr::pull()
 
   # try to map group ID to the data strata
   # all strata should be factors and therefore can just extract the
   color_label <-
     plot_build$plot$data %>%
-    dplyr::pull(.data$strata) |>
+    dplyr::pull(.data$strata) %>%
     levels()
 
   # i hope this will work 100% of the time!
   # waht modifications could a use make the the figures that could break this connection?
-  colors |> setNames(color_label)
+  colors %>% setNames(color_label)
 }
 
-.construct_color_block <- function() {
+.construct_color_block <- function(gg, color_block_mapping) {
+  plot_range <-
+    ggplot2::ggplot_build(gg)$layout$panel_params[[1]]$x.range
+
   list(
-    ggplot2::theme(axis.text.y = ggplot2::element_text(family = "mono"))
+    # ggplot2::coord_cartesian(clip = "off"),
+    ggplot2::theme(
+      axis.text.y.left = ggplot2::element_text(color = color_block_mapping, fill = color_block_mapping)
+    ),
+
+    # ggplot2::geom_label(
+    #   ggplot2::aes(fill = .data[[y_value]]),
+    #   label = "BLUE",
+    #   hjust = 1,
+    #   label.r = ggplot2::unit(0.25, "lines"),
+    #   x = plot_range[1] - .05 * diff(plot_range) - .25,
+    #   color = "transparent"
+    # )
   )
 }
