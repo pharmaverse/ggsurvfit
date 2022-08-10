91f58a52fc941fa066e6e67959b10b57c6aa08cc
Mark Baillie <bailliem@gmail.com>
2022-08-10 12:56:41 +0200

    WIP: clean up labels

--- R/data.R
+++ R/data.R
@@ -6,11 +6,10 @@
 "df_lung"
 
 
-#' ADTTE survival data set
+#' ADTTE example survival data set
 #'
-#' This is a copy of the lung data set exported by the survival
-#' package. This data set, however, has column labels and
-#' numeric categorical variables are now factors with assigned levels.
+#' This is a version of the data from here https://github.com/VIS-SIG/Wonderful-Wednesdays/tree/master/data/2020/2020-04-08
+#'
 "adtte"
 
 
