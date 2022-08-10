07385787ec273dc8ff82170f9418c7761e273ecb
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-08-09 22:10:21 -0400

    test updates

---
+++ tests/testthat/test-add_risktable_strata_symbol.R
@@ -0,0 +1,25 @@
+test_that("add_risktable_strata_symbol() works", {
+  expect_error(
+    p <-
+      survfit2(Surv(time, status) ~ sex, data = df_lung) %>%
+      ggsurvfit(size = 1) +
+      add_confidence_interval() +
+      add_risktable(risktable_group = "risktable_stats"),
+    NA
+  )
+  vdiffr::expect_doppelganger(
+    "add_risktable_strata_symbol-default",
+    p + add_risktable_strata_symbol()
+  )
+  vdiffr::expect_doppelganger(
+    "add_risktable_strata_symbol-circle",
+    p + add_risktable_strata_symbol(symbol = "\U25CF")
+  )
+
+  expect_message(
+    print(survfit2(Surv(time, status) ~ sex, data = df_lung) %>%
+            ggsurvfit(size = 1) +
+            add_confidence_interval() +
+            add_risktable_strata_symbol(risktable_group = "risktable_stats"))
+  )
+})
