a5d807a44c9f634eb7430737fe2684f56227c403
Mark Baillie <bailliem@gmail.com>
2022-08-10 12:47:59 +0200

    WIP: clean up data Add source

--- R/data.R
+++ R/data.R
@@ -4,3 +4,13 @@
 #' package. This data set, however, has column labels and
 #' numeric categorical variables are now factors with assigned levels.
 "df_lung"
+
+
+#' ADTTE survival data set
+#'
+#' This is a copy of the lung data set exported by the survival
+#' package. This data set, however, has column labels and
+#' numeric categorical variables are now factors with assigned levels.
+"adtte"
+
+
---
+++ data-raw/01-adtte.R
@@ -0,0 +1,34 @@
+#source data https://github.com/VIS-SIG/Wonderful-Wednesdays/tree/master/data/2020/2020-04-08
+
+# load data - local version
+# variable names toLowerCase
+adtte <-
+  readr::read_csv('data-raw/2020-04-08-psi-vissig-adtte.csv',
+                  show_col_types = FALSE) |>
+  #janitor::clean_names() |>
+  dplyr::as_tibble()
+
+
+# attach var labels
+attr(adtte$STUDYID, "label") <- "Study identifier"
+attr(adtte$USUBJID, "label") <- "Unique patient identifier"
+attr(adtte$AGE, "label") <- "Age at randomisation [years]"
+attr(adtte$STR01, "label") <- "Hormone receptor status at randomisation"
+attr(adtte$STR01N, "label") <- "Hormone receptor positive (Numeric)"
+attr(adtte$STR01L, "label") <- "Hormone receptor positive (Long format)"
+attr(adtte$STR02, "label") <- "Prior Radiotherapy at randomisationr"
+attr(adtte$STR02N, "label") <- "Prior Radiotherapy at randomisation (Numeric)"
+attr(adtte$STR02L, "label") <- "Prior Radiotherapy at randomisation (Long format)"
+attr(adtte$TRT01P, "label") <- "Planned treatment assigned at randomisation"
+attr(adtte$TRT01PN, "label") <- "Planned treatment assigned at randomisation (Numeric)"
+attr(adtte$PARAM, "label") <- "Analysis parameter: Progression free survival"
+attr(adtte$PARAMCD, "label") <- "Analysis parameter code"
+attr(adtte$AVAL, "label") <- "Analysis value (time to event [days])"
+attr(adtte$CNSR, "label") <- "Censoring flag (0 = Event, 1 = censored)"
+attr(adtte$EVNTDESC, "label") <- "Event description"
+attr(adtte$CNSDTDSC, "label") <- " Censoring description"
+attr(adtte$DCTREAS, "label") <- "Discontinuation from study reason"
+
+
+# save to data
+usethis::use_data(adtte, overwrite = TRUE, internal = FALSE)
