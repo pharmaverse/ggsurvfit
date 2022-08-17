ffa6f6d5c53bffebe2e011f9b5be93465844d8bf
Kirina Sirohi <sirohik@mskcc.org>
2022-08-17 14:10:40 -0400

    finished tests

--- tests/testthat/test-add_risktable_strata_symbol.R
+++ tests/testthat/test-add_risktable_strata_symbol.R
@@ -48,6 +48,7 @@ test_that("add_risktable_strata_symbol() works", {
 
 
 test_that(".match_strata_level_to_color() works", {
+  # survfit2 - Factor-----------------------------------------------------------
   expect_equal(
     survfit2(Surv(time, status) ~ sex, data = df_lung) %>%
       ggsurvfit() %>%
@@ -59,6 +60,240 @@ test_that(".match_strata_level_to_color() works", {
     c(Male = "#F8766D",   # red
       Female = "#00BFC4") # blue
   )
+  # survfit - Factor -----------------------------------------------------------
+  expect_equal(
+    survfit(Surv(time, status) ~ sex, data = df_lung) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("sex=Male" = "#F8766D",   # red
+      "sex=Female" = "#00BFC4") # blue
+  )
+  # survfit2 - Numeric ---------------------------------------------------------
+  expect_equal(
+    survfit2(Surv(time, status) ~ as.numeric(sex), data = df_lung) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("1" = "#F8766D",   # red
+      "2" = "#00BFC4") # blue
+  )
+  # survfit - Numeric ----------------------------------------------------------
+  expect_equal(
+    survfit(Surv(time, status) ~ as.numeric(sex), data = df_lung) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("as.numeric(sex)=1" = "#F8766D",   # red
+      "as.numeric(sex)=2" = "#00BFC4") # blue
+  )
+  # survfit2 - Character -------------------------------------------------------
+  expect_equal(
+    survfit2(Surv(time, status) ~ as.character(sex), data = df_lung) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("Female" = "#F8766D",   # red
+      "Male" = "#00BFC4") # blue
+  )
+  # survfit - Character --------------------------------------------------------
+  expect_equal(
+    survfit(Surv(time, status) ~ as.character(sex), data = df_lung) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("as.character(sex)=Female" = "#F8766D",   # red
+      "as.character(sex)=Male" = "#00BFC4") # blue
+  )
+  # survfit2 - Glue ------------------------------------------------------------
+  expect_equal(
+    survfit2(Surv(time, status) ~ sex,
+             data = df_lung %>% dplyr::mutate(sex = glue::glue("{as.character(sex)}"))) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c(Female = "#F8766D",   # red
+      Male = "#00BFC4") # blue
+  )
+  # survfit - Glue -------------------------------------------------------------
+  expect_equal(
+    survfit(Surv(time, status) ~ sex,
+             data = df_lung %>% dplyr::mutate(sex = glue::glue("{as.character(sex)}"))) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("sex=Female" = "#F8766D",   # red
+      "sex=Male" = "#00BFC4") # blue
+  )
+  # survfit2 - Ordered factor --------------------------------------------------
+  expect_equal(
+    survfit2(Surv(time, status) ~ factor(sex, ordered = TRUE), data = df_lung) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("Male" = "#F8766D",   # red
+      "Female" = "#00BFC4") # blue
+  )
+  # survfit - Ordered factor ---------------------------------------------------
+  expect_equal(
+    survfit(Surv(time, status) ~ factor(sex, ordered = TRUE), data = df_lung) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("factor(sex, ordered = TRUE)=Male" = "#F8766D",   # red
+      "factor(sex, ordered = TRUE)=Female" = "#00BFC4") # blue
+  )
+  # survfit2 - factors whose levels sort in alphabetical order -----------------
+  expect_equal(
+    survfit2(Surv(time, status) ~ sex,
+            data = df_lung %>% dplyr::mutate(sex = factor(sex, levels = c("Female", "Male")))) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("Female" = "#F8766D",   # red
+      "Male" = "#00BFC4") # blue
+  )
+  # survfit - factors whose levels sort in alphabetical order ------------------
+  expect_equal(
+    survfit(Surv(time, status) ~ sex,
+             data = df_lung %>% dplyr::mutate(sex = factor(sex, levels = c("Female", "Male")))) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("sex=Female" = "#F8766D",   # red
+      "sex=Male" = "#00BFC4") # blue
+  )
+  # survfit2 - factors whose levels do not sort alphabetically -----------------
+  expect_equal(
+    survfit2(Surv(time, status) ~ sex,
+            data = df_lung %>% dplyr::mutate(sex = factor(sex, levels = c("Male", "Female")))) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("Male" = "#F8766D",   # red
+      "Female" = "#00BFC4") # blue
+  )
+  # survfit - factors whose levels do not sort alphabetically ------------------
+  expect_equal(
+    survfit(Surv(time, status) ~ sex,
+             data = df_lung %>% dplyr::mutate(sex = factor(sex, levels = c("Male", "Female")))) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("sex=Male" = "#F8766D",   # red
+      "sex=Female" = "#00BFC4") # blue
+  )
+  # survfit2 - factors with unobserved levels ----------------------------------
+  expect_equal(
+    survfit2(Surv(time, status) ~ ph.ecog, data = df_lung) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("Asymptomatic" = "#F8766D", # red
+      "Symptomatic and ambulatory" = "#7CAE00", # green
+      "In bed <50% of the day" = "#00BFC4", # blue
+      "In bed > 50% of the day" = "#C77CFF") # purple
+  )
+  # survfit - factors with unobserved levels -----------------------------------
+  expect_equal(
+    survfit(Surv(time, status) ~ ph.ecog, data = df_lung) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("ph.ecog=Asymptomatic" = "#F8766D", # red
+      "ph.ecog=Symptomatic and ambulatory" = "#7CAE00", # green
+      "ph.ecog=In bed <50% of the day" = "#00BFC4", # blue
+      "ph.ecog=In bed > 50% of the day" = "#C77CFF") # purple
+  )
+  # survfit2 - No strata level -------------------------------------------------
+  expect_equal(
+    survfit2(Surv(time, status) ~ 1, data = df_lung) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("Overall" = "black") # black
+  )
+  # survfit - No strata level --------------------------------------------------
+  expect_equal(
+    survfit(Surv(time, status) ~ 1, data = df_lung) %>%
+      ggsurvfit() %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("Overall" = "black") # black
+  )
+  # survfit2 - color_scale_manual ----------------------------------------------
+  expect_equal(
+    survfit2(Surv(time, status) ~ 1, data = df_lung) %>%
+      ggsurvfit(color = "#00BFC4") %>%
+    ggplot2::ggplot_build() %>%
+    .match_strata_level_to_color(
+      risktable_group = "risktable_stats",
+      risktable_symbol_args = list(symbol = "\U25AC")
+    ),
+    c("Overall" = "#00BFC4") # blue
+  )
+  # survfit - color_scale_manual ----------------------------------------------
+  expect_equal(
+    survfit(Surv(time, status) ~ 1, data = df_lung) %>%
+      ggsurvfit(color = "#00BFC4") %>%
+      ggplot2::ggplot_build() %>%
+      .match_strata_level_to_color(
+        risktable_group = "risktable_stats",
+        risktable_symbol_args = list(symbol = "\U25AC")
+      ),
+    c("Overall" = "#00BFC4") # blue
+  )
 
   # Add more tests using the following:
   # - survfit() and survfit2()
@@ -66,7 +301,7 @@ test_that(".match_strata_level_to_color() works", {
   #      factor, ordered factor, factors whose levels sort in alphabetical order,
   #      factors whose levels do not sort alphabetically,
   #      factors with unobserved levels
-  # - no strata level, e.g. sSurv(time, status) ~ 1
+  # - no strata level, e.g. Surv(time, status) ~ 1
   # - when the colors have been changed by the user with `scale_color_manual()`
   # - when two or more levels have been assigned the same color
 
