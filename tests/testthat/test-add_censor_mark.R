36df0a69b5c57f9390a46daf915872721dc62890
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-08-20 20:49:44 -0400

    updates

--- tests/testthat/test-add_censor_mark.R
+++ tests/testthat/test-add_censor_mark.R
@@ -42,16 +42,6 @@ test_that("add_censor_mark() works with ggcuminc()", {
   vdiffr::expect_doppelganger("cuminc1-censor_mark", lst_cuminc_censor_mark[[1]])
   vdiffr::expect_doppelganger("cuminc2-censor_mark", lst_cuminc_censor_mark[[2]])
   vdiffr::expect_doppelganger("cuminc3-censor_mark", lst_cuminc_censor_mark[[3]])
-
-  expect_error(
-    lst_cuminc_censor_mark_outcome <-
-      list(cuminc1, cuminc2, cuminc3) %>%
-      lapply(function(x) ggcuminc(x, outcome = c("death from cancer", "death other causes")) + add_censor_mark()),
-    NA
-  )
-  vdiffr::expect_doppelganger("cuminc1-censor_mark-all-outcomes", lst_cuminc_censor_mark_outcome[[1]])
-  vdiffr::expect_doppelganger("cuminc2-censor_mark-all-outcomes", lst_cuminc_censor_mark_outcome[[2]])
-  vdiffr::expect_doppelganger("cuminc3-censor_mark-all-outcomes", lst_cuminc_censor_mark_outcome[[3]])
 })
 
 test_that("add_censor_mark() works with ggcuminc()", {
@@ -65,4 +55,3 @@ test_that("add_censor_mark() works with ggcuminc()", {
   vdiffr::expect_doppelganger("cuminc2-censor_mark-all-outcomes", lst_cuminc_censor_mark_outcome[[2]])
   vdiffr::expect_doppelganger("cuminc3-censor_mark-all-outcomes", lst_cuminc_censor_mark_outcome[[3]])
 })
-
