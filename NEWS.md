a46053f1d80a0c11d79aacb54bf1c17dbd6a4b9a
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-09-11 14:55:53 -0400

    Adding `add_quanitile(x_value=)` argument (#81)
    * adding x_value to add_quanitile()
    
    * increment version number
    
    * updating default values
    
    * doc update
    
    * unit tests and error handling
    
    * specify all_of
    
    * Increment version number to 0.1.0.9005
    
    Co-authored-by: Mark Baillie <bailliem@gmail.com>

--- NEWS.md
+++ NEWS.md
@@ -1,5 +1,7 @@
 # ggsurvfit (development version)
 
+* Added the `add_quantile(x_value=)` argument that places line segments at the time specified.
+
 * Added the `scale_continuous_reduce_pad()` function that wraps both `ggplot2::scale_x_continuous()` and `ggplot2::scale_y_continuous()` and uses reduced padding (via the `expand=` argument) to both scales. (#82)
 
 * Package now depends on {ggplot2}, meaning that it'll be attached anytime {ggsurvfit} is attached. (#62)
--- R/add_quantile.R
+++ R/add_quantile.R
@@ -2,7 +2,8 @@
 #'
 #' Add quantile information annotated on to the plot.
 #'
-#' @param y_value Numeric value where the line segment will be drawn. Default is `0.5`
+#' @param y_value,x_value Numeric value where the line segment will be drawn.
+#' Default is `y_value=0.5` when both `y_value` and `x_value` are unassigned.
 #' @param ... Named arguments passed to `ggplot2::geom_segment()` with default `linetype = 2`
 #'
 #' @return a ggplot2 figure
@@ -17,13 +18,17 @@
 #'   ggsurvfit() +
 #'   add_quantile(linetype = 2) +
 #'   add_quantile(y_value = 0.9, linetype = 3)
-add_quantile <- function(y_value = 0.5, ...) {
+#'
+#' survfit2(Surv(time, status) ~ sex, data = df_lung) %>%
+#'   ggsurvfit() +
+#'   add_quantile(linetype = 2, y_value = NULL, x_value = 10)
+add_quantile <- function(y_value = NULL, x_value = NULL, ...) {
   ggplot2::layer(
     stat = StatQuantileSurvfit, data = NULL, mapping = NULL, geom = "segment",
     position = "identity", show.legend = NA, inherit.aes = TRUE,
     params =
       c(
-        list(y_value = y_value),
+        list(y_value = y_value, x_value = x_value),
         utils::modifyList(x = list(linetype = 2, na.rm = FALSE),
                           val = rlang::dots_list(...))
       )
@@ -35,14 +40,109 @@ StatQuantileSurvfit <-
     "StatQuantileSurvfit",
     ggplot2::Stat,
     compute_panel =
-      function(data, scales, params, y_value = 0.5) {
+      function(data, scales, params, y_value, x_value) {
         .is_ggsurvfit(data, fun_name = "add_quantile()", required_aes_cols = c("x", "y"))
-        quantile_km_in_stat(data, y_value)
+        quantile_km_in_stat(data, y_value, x_value)
       }
   )
 
 
-quantile_km_in_stat <- function(data, y_value) {
+quantile_km_in_stat <- function(data, y_value, x_value) {
+  if (is.null(y_value) && is.null(x_value)) y_value <- 0.5 # assign default value
+  if (length(y_value) > 1 || length(x_value) > 1)
+    cli_abort(c(
+      "!" = "Neither {.code y_value} nor {.code x_value} may have length greater than one.",
+      "i" = "To plot multiple quantiles, call {.code add_quantile()} multiple times."
+    ))
+
+
+  df_quantile_y <- .create_y_value_df(data, y_value)
+  df_quantile_x <- .create_x_value_df(data, x_value)
+
+
+  dplyr::bind_rows(
+    df_quantile_y,
+    df_quantile_x
+  )
+}
+
+.create_x_value_df <- function(data, x_value) {
+  if (is.null(x_value))
+    return(
+      dplyr::tibble(x = numeric(), y = numeric(),
+                    xend = numeric(), yend = numeric())
+    )
+
+  # creating df with the horizontal line definition
+  df_quantile <-
+    data %>%
+    dplyr::select(dplyr::all_of(c("x", "y", "group"))) %>%
+    dplyr::group_by(dplyr::across(dplyr::any_of("group"))) %>%
+    dplyr::mutate(
+      x_max = max(.data$x)
+    ) %>%
+    dplyr::ungroup()
+
+  # create tibble of times
+  df_times <-
+    dplyr::tibble(x = list(x_value), group = unique(data$group)) %>%
+    tidyr::unnest(cols = .data$x)
+
+  # merge tibble of times with tidy df
+  df_quantile <-
+    dplyr::full_join(
+      df_quantile,
+      df_times,
+      by = c("x", "group")
+    ) %>%
+    dplyr::arrange(dplyr::across(dplyr::any_of(c("group", "x"))))
+
+  # fill in missing stats
+  df_quantile <-
+    df_quantile %>%
+    dplyr::group_by(dplyr::across(dplyr::any_of("group"))) %>%
+    tidyr::fill(.data$y, .data$x_max, .direction = "down") %>%
+    dplyr::ungroup()
+
+  # any times above the max observed time are set to NA
+  df_quantile <-
+    df_quantile %>%
+    dplyr::mutate(
+      dplyr::across(
+        dplyr::any_of("y"),
+        ~ ifelse(.data$x > .data$x_max, NA, .)
+      )
+    ) %>%
+    dplyr::filter(.data$x %in% .env$x_value) %>%
+    dplyr::select(dplyr::all_of(c("x", "y"))) %>%
+    dplyr::mutate(
+      xend = 0,
+      yend = .data$y
+    ) %>%
+    dplyr::filter(stats::complete.cases(.))
+
+  # add row for vertical line segment
+  if (nrow(df_quantile) > 0) {
+    df_quantile <-
+      df_quantile %>%
+      dplyr::bind_rows(
+        dplyr::tibble(
+          x = x_value, y = 0,
+          xend = x_value, yend = max(df_quantile$y)
+        )
+      )
+  }
+
+  df_quantile
+}
+
+.create_y_value_df <- function(data, y_value) {
+  if (is.null(y_value))
+    return(
+      dplyr::tibble(x = numeric(), y = numeric(),
+                    xend = numeric(), yend = numeric())
+    )
+
   # create vertical line segments
   df_quantile <-
     data %>%
