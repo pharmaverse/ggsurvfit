6856968a2fc452813f3ee179b1bab982f9a0109c
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-09-21 22:06:43 -0400

    Update test-survfit2.R

--- tests/testthat/test-survfit2.R
+++ tests/testthat/test-survfit2.R
@@ -9,6 +9,15 @@ test_that("survfit2() works", {
   )
 
   expect_identical(
+    survfit2(Surv(time, status) ~ 1, data = survival::lung, cluster = ph.karno) %>%
+      modifyList(val = list(call = NULL, .Environment = NULL)) %>%
+      unclass(),
+    survival::survfit(Surv(time, status) ~ 1, data = survival::lung, cluster = ph.karno) %>%
+      modifyList(val = list(call = NULL)) %>%
+      unclass()
+  )
+
+  expect_identical(
     survfit2(Surv(survival::lung$time, survival::lung$status) ~ 1) %>%
       modifyList(val = list(call = NULL, .Environment = NULL)) %>%
       unclass(),
