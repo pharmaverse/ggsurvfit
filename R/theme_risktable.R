2d51346cad95ceb1d45d3148ff36d3645b8a4626
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-10-07 11:08:34 -0400

    in progress

--- R/theme_risktable.R
+++ R/theme_risktable.R
@@ -3,6 +3,9 @@
 #' @description
 #' Returns ggplot list of calls defining a theme meant to be applied to a risk table.
 #'
+#' @param axis.text.y.size text size of the labels on the right of the risk table
+#' @param plot.title.size text size of the risk table title
+#'
 #' @name theme_risktable
 #' @return a ggplot2 figure
 #' @examples
@@ -20,24 +23,20 @@ NULL
 
 #' @export
 #' @name theme_risktable
-theme_risktable_default <- function() {
+theme_risktable_default <- function(axis.text.y.size = 10, plot.title.size = 10.75) {
   list(
     ggplot2::theme_bw(),
     ggplot2::theme(
-      axis.title.x = ggplot2::element_text(
-        size = 9,
-        vjust = 1,
-        hjust = 1
-      ),
+      axis.title.x = ggplot2::element_text(size = 9, vjust = 1, hjust = 1),
       panel.grid.major = ggplot2::element_blank(),
       panel.grid.minor = ggplot2::element_blank(),
       panel.border = ggplot2::element_blank(),
       axis.line = ggplot2::element_blank(),
       axis.text.x = ggplot2::element_blank(),
       axis.ticks = ggplot2::element_blank(),
-      axis.text.y = ggplot2::element_text(size = 8, colour = "black", face = "plain"),
+      axis.text.y = ggplot2::element_text(size = axis.text.y.size, colour = "black", face = "plain"),
       plot.margin = ggplot2::unit(c(1, 0, 0, 0), "lines"),
-      plot.title = ggplot2::element_text(hjust = 0, vjust = 0, size = 10),
+      plot.title = ggplot2::element_text(hjust = 0, vjust = 0, size = plot.title.size),
       legend.position = "none"
     ),
     ggplot2::xlab(NULL),
