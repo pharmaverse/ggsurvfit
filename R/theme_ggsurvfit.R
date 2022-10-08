c77dfdd7457cf5cb4098bc3b2747fd9a4442c948
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-10-07 21:23:25 -0400

    Risktable Sizes (#108)
    * in progress
    
    * increased default text size
    
    * update

--- R/theme_ggsurvfit.R
+++ R/theme_ggsurvfit.R
@@ -2,6 +2,8 @@
 #'
 #' @description
 #' Returns ggplot list of calls defining a theme.
+#' - `theme_ggsurvfit_default()`: Builds on `theme_bw()` with increased text sizes.
+#' - `theme_ggsurvfit_KMunicate()`: Theme to create [KMunicate-styled](http://dx.doi.org/10.1136/bmjopen-2019-030215) figures.
 #'
 #' @return a ggplot2 theme
 #' @name theme_ggsurvfit
@@ -16,7 +18,10 @@ theme_ggsurvfit_default <- function() {
   list(
     ggplot2::theme_bw(),
     ggplot2::theme(legend.position = "bottom"),
-    ggplot2::theme(strip.background = ggplot2::element_blank())
+    ggplot2::theme(strip.background = ggplot2::element_blank()),
+    ggplot2::theme(axis.text = ggplot2::element_text(size = 10)),
+    ggplot2::theme(axis.title = ggplot2::element_text(size = 12)),
+    ggplot2::theme(legend.text = ggplot2::element_text(size = 10))
   )
 }
 
--- R/theme_risktable.R
+++ R/theme_risktable.R
@@ -3,6 +3,9 @@
 #' @description
 #' Returns ggplot list of calls defining a theme meant to be applied to a risk table.
 #'
+#' @param axis.text.y.size text size of the labels on the left of the risk table
+#' @param plot.title.size text size of the risk table title
+#'
 #' @name theme_risktable
 #' @return a ggplot2 figure
 #' @examples
@@ -10,6 +13,14 @@
 #'
 #' # default ------------------------------------
 #' p + add_risktable(theme = theme_risktable_default())
+#'
+#' # larger text --------------------------------
+#' p +
+#'   add_risktable(
+#'     size = 4,
+#'     theme = theme_risktable_default(axis.text.y.size = 12,
+#'                                     plot.title.size = 14)
+#'   )
 #'
 #' # boxed --------------------------------------
 #' p + add_risktable(theme = theme_risktable_boxed())
@@ -20,24 +31,20 @@ NULL
 
 #' @export
 #' @name theme_risktable
-theme_risktable_default <- function() {
+theme_risktable_default <- function(axis.text.y.size = 10, plot.title.size = 10.75) {
   list(
     ggplot2::theme_bw(),
     ggplot2::theme(
-      axis.title.x = ggplot2::element_text(
-        size = 9,
-        vjust = 1,
-        hjust = 1
-      ),
+      axis.title.x = ggplot2::element_text(size = 9, vjust = 1, hjust = 1),
       panel.grid.major = ggplot2::element_blank(),
       panel.grid.minor = ggplot2::element_blank(),
       panel.border = ggplot2::element_blank(),
       axis.line = ggplot2::element_blank(),
       axis.text.x = ggplot2::element_blank(),
       axis.ticks = ggplot2::element_blank(),
-      axis.text.y = ggplot2::element_text(size = 8, colour = "black", face = "plain"),
+      axis.text.y = ggplot2::element_text(size = axis.text.y.size, colour = "black", face = "plain"),
       plot.margin = ggplot2::unit(c(1, 0, 0, 0), "lines"),
-      plot.title = ggplot2::element_text(hjust = 0, vjust = 0, size = 10),
+      plot.title = ggplot2::element_text(hjust = 0, vjust = 0, size = plot.title.size),
       legend.position = "none"
     ),
     ggplot2::xlab(NULL),
@@ -47,9 +54,10 @@ theme_risktable_default <- function() {
 
 #' @export
 #' @name theme_risktable
-theme_risktable_boxed <- function() {
+theme_risktable_boxed <- function(axis.text.y.size = 10, plot.title.size = 10.75) {
   list(
-    theme_risktable_default(),
+    theme_risktable_default(axis.text.y.size = axis.text.y.size,
+                            plot.title.size = plot.title.size),
     ggplot2::theme(panel.border = ggplot2::element_rect(fill = NA))
   )
 }
