76e496f08f38d6835e16f0caa67aa5913beab461
Daniel Sjoberg <danield.sjoberg@gmail.com>
2022-10-09 08:16:47 -0400

    improvements to scales_ggsurvfit

--- R/scale_ggsurvfit.R
+++ R/scale_ggsurvfit.R
@@ -9,6 +9,8 @@
 #'
 #' If you use this function, you **must** include all scale specifications
 #' that would appear in `scale_x_continuous()` or `scale_y_continuous()`.
+#' For example, it's common that you'll need to specify the x-axis break points.
+#' `scale_ggsurvfit(x_scales=list(breaks=0:9))`.
 #'
 #' To reset any of the above settings to their ggplot2 default, set the value
 #' to `NULL`, e.g. `y_scales = list(limits = NULL)`.
@@ -19,6 +21,7 @@
 #' @return a ggplot2 figure
 #' @export
 #'
+#' @rdname scale_ggsurvfit
 #' @examples
 #' ggsurvfit <-
 #'   survfit2(Surv(time, status) ~ surg, data = df_colon) %>%
@@ -34,17 +37,38 @@
 #'     x_scales = list(breaks = 0:9),
 #'     y_scales = list(label = scales::percent, limits = c(0, 1))
 #'   )
-scale_ggsurvfit <- function(x_scales = list(), y_scales = list()) {
-  list(
-    rlang::inject(ggplot2::scale_x_continuous(
-      !!!utils::modifyList(x = list(expand = c(0.015, 0)),
-                           val = x_scales)
-    )),
-    rlang::inject(ggplot2::scale_y_continuous(
-      !!!utils::modifyList(x = list(expand = c(0.025, 0),
-                                    label = scales::label_percent(),
-                                    limits = c(0, 1)),
-                           val = y_scales)
-    ))
-  )
+scale_ggsurvfit <- function(x_scales = list(), y_scales = list()){
+  scale_ggsurvfit_empty_list <- list()
+  structure(scale_ggsurvfit_empty_list, x_scales = x_scales, y_scales = y_scales, class = "scale_ggsurvfit")
 }
+
+
+#' @export
+ggplot_add.scale_ggsurvfit <- function (object, plot, object_name) {
+  update_scale_ggsurvfit(plot, object)
+}
+
+update_scale_ggsurvfit <- function(p, scale_ggsurvfit_empty_list) {
+  # setting default arguments
+  x_scale_defaults <- list(expand = c(0.015, 0))
+  y_scale_defaults <- list(expand = c(0.025, 0), label = scales::label_percent())
+  # set limits of a survival curve
+  if (isTRUE(p$data$estimate_type[1] == "survival"))
+    y_scale_defaults <- c(y_scale_defaults, list(limits = c(0, 1)))
+
+  # getting user-passed arguments
+  x_scales <- attr(scale_ggsurvfit_empty_list, "x_scales")
+  y_scales <- attr(scale_ggsurvfit_empty_list, "y_scales")
+
+  # apply scales
+  p +
+    list(
+      rlang::inject(ggplot2::scale_x_continuous(
+        !!!utils::modifyList(x = x_scale_defaults, val = x_scales %||% list())
+      )),
+      rlang::inject(ggplot2::scale_y_continuous(
+        !!!utils::modifyList(x = y_scale_defaults, val = y_scales %||% list())
+      ))
+    )
+}
+
