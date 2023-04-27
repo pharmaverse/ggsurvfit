6d49d97240639f4bd5bd5b5d8a67ee91298cc7cf
Daniel Sjoberg <danield.sjoberg@gmail.com>
2023-04-27 13:24:15 -0400

    Update helper-r-version.R

--- tests/testthat/helper-r-version.R
+++ tests/testthat/helper-r-version.R
@@ -2,10 +2,19 @@
 # because there are sometimes changes across all R versions/OSs
 current_release_version <-
   tryCatch(
-    read.dcf(url("https://cran.r-project.org/src/base/VERSION-INFO.dcf"))[1,][1] |>
-      unname(),
+    read.dcf(url("https://cran.r-project.org/src/base/VERSION-INFO.dcf"))[1,][1] %>%
+      unname() %>%
+      {regmatches(., m = gregexpr("^\\d\\.\\d", .))} |>
+      unlist(),
     error = function(e) character(0L)
   )
 
 loaded_release_version <-
-  paste0(R.version$major, ".", R.version$minor)
+  paste(
+    R.version$major,
+    R.version$minor %>%
+      {regmatches(., m = gregexpr("^\\d", .))} |>
+      unlist(),
+    sep = "."
+  )
+
