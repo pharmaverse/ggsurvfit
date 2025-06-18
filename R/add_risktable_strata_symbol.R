63e9cde2c0ad2558b2fbd4b94a33375d76477abf
Teun van den Brand <tahvdbrand@gmail.com>
2025-06-18 13:28:29 +0200

    remove `element_text2()` hack

--- R/add_risktable_strata_symbol.R
+++ R/add_risktable_strata_symbol.R
@@ -102,32 +102,7 @@ update_add_risktable_strata_symbol <- function(p, add_risktable_strata_symbol_em
     ),
     ggplot2::theme(
       axis.text.y.left =
-        element_text2(color = rev(color_block_mapping), ...)
+        ggplot2::element_text(color = rev(color_block_mapping), ...)
     )
   )
 }
-
-# taken from SO https://stackoverflow.com/questions/73293798
-element_text2 <- function(..., color = NULL) {
-  # Explicitly don't pass colour
-  # Note: user can still pass `colour`, but I'm not here to write perfect code,
-  # just to give a working example
-  elem <- ggplot2::element_text(...)
-  elem$colour <- color # Assign after element is constructed
-  class(elem) <- c("element_text2", "element_text", "element") # Re-class
-  elem
-}
-
-# these lines were not getting hit on code covereage so i comment them out and it still works :)
-# # S3 Method for your custom class' drawing code
-# element_grob.element_text2 <- function(element, label = "", ...,
-#                                        colour = NULL) {
-#   # Repeat colour to match length of label, if colour exists
-#   if (length(colour)) {
-#     colour <- rep_len(colour, length(label))
-#   }
-#   # Re-class to old class
-#   class(element) <- c("element_text", "element")
-#   # Call element_grob.element_text method
-#   ggplot2::element_grob(element, label = label, ..., colour = colour)
-# }
