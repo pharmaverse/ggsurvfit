e00e4ad436cc9952528447510cb2aa3c5d820ab8
ShreyaSreeram27 <sreeras4@gene.com>
2025-07-23 09:43:35 -0700

    added in new error message

--- R/ggcuminc.R
+++ R/ggcuminc.R
@@ -57,6 +57,13 @@ ggcuminc <- function(x, outcome = NULL,
     )
   }
 
+   # Generate an error if the input is a `survfit.coxphms` regression model: ----
+  if (inherits(x, "survfitcoxms")) {
+    cli_abort(
+      "Argument {.arg x} does not support {.cls survfit.coxphms} object."
+    )
+  }
+
   # prep data to be passed to ggplot() -----------------------------------------
   if (inherits(x, "tidycuminc")) {
     df <- tidy_cuminc(x = x)
