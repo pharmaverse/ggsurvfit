f597d98ea9a97f044df8b2a92eded455c1d18b90
ShreyaSreeram27 <sreeras4@gene.com>
2025-10-03 15:34:00 -0700

    change to pull the subset argument (#267)
    * change to pull the subset argument
    
    * doc updates
    
    * Updated construction of survdiff() call
    
    * unit test for the subset arg
    
    ---------
    
    Co-authored-by: Daniel D. Sjoberg <danield.sjoberg@gmail.com>

--- tests/testthat/test-survfit2_p.R
+++ tests/testthat/test-survfit2_p.R
@@ -38,3 +38,39 @@ test_that("survfit2_p() throws error", {
       add_pvalue()
   )
 })
+
+test_that("survfit2_p() works with subset argument", {
+  #create survfit object with subset argument
+  sf_subset <- survfit2(Surv(time, status) ~ sex, data = df_lung, subset = age >= 75)
+
+  #calculate expectes p val directly with survdiff using the same subset
+  expected_p <- survival::survdiff(
+    Surv(time, status) ~ sex,
+    data = df_lung,
+    subset = age >= 75
+  ) %>%
+    broom::glance() %>%
+    dplyr::pull(p.value) %>%
+    format_p()
+
+  #testing that survfit2_p respects the subset argument
+  expect_equal(
+    survfit2_p(sf_subset, prepend_p = FALSE),
+    expected_p
+  )
+
+  #the subset p-value should be different from full data p-value
+  sf_full <- survfit2(Surv(time, status) ~ sex, data = df_lung)
+
+  expect_false(
+    survfit2_p(sf_subset, prepend_p = FALSE) == survfit2_p(sf_full, prepend_p = FALSE)
+  )
+
+  #testing that subset using filter() gives same result as subset argument
+  sf_filtered <- survfit2(Surv(time, status) ~ sex, data = df_lung %>% dplyr::filter(age >= 75))
+
+  expect_equal(
+    survfit2_p(sf_subset, prepend_p = FALSE),
+    survfit2_p(sf_filtered, prepend_p = FALSE)
+  )
+})
